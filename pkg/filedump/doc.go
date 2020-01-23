74062d07de0b02d18e68498f50459a49703ed30c
Jose Selvi <jselvi@pentester.es>
2020-01-23 12:17:59 +0100

    Adding reference to license

--- pkg/filedump/doc.go
+++ pkg/filedump/doc.go
@@ -1,3 +1,13 @@
+/*
+Released as open source by NCC Group Plc - http://www.nccgroup.com/
+
+Developed by Jose Selvi, jose dot selvi at nccgroup dot com
+
+http://www.github.com/nccgroup/wstalker
+
+Released under AGPL see LICENSE for more information
+*/
+
 package filedump
 
 // Package filedump is...
